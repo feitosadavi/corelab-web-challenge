7715204668f937d8ca6bc5954f9192832efc1737
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-04 23:32:30 -0300

    test: ensure Search display the placeholder passed throught props

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,15 +1,20 @@
-// Button.spec.tsx
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Search from '.';
 
-describe('Card component tests', () => {
+describe('Search component tests', () => {
   const makeSut = () => (<Search placeholder='placeholder_text' value='search_value' onChange={() => true} />)
 
-  test('should Card be in the document', () => {
+  test('should Search be in the document', () => {
     const sut = makeSut()
     render(sut);
     const search = screen.getByTestId('search')
     expect(search).toBeInTheDocument();
+  });
+  test('should Search display the placeholder passed throught props', () => {
+    const sut = makeSut()
+    render(sut);
+    const placeHolderText = screen.queryByPlaceholderText(/placeholder_text/i)
+    expect(placeHolderText).toBeInTheDocument();
   });
 });
