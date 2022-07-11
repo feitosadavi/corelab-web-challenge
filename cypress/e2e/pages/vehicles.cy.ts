3b2e21726a1e204ad5757c7695733fe1d64e1f0b
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-11 15:24:48 -0300

    refactor(cypress)!: add pages e2e tests to a separeted folder

--- cypress/e2e/pages/vehicles.cy.ts
+++ cypress/e2e/pages/vehicles.cy.ts
@@ -5,7 +5,7 @@ describe('Vehicles page', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('should change search input value on search ', () => {
+  it('should change search input value on search', () => {
     cy.get('input').should('be.empty')
     cy.get('input').type('any_value')
     cy.get('input').should('have.value', 'any_value')
