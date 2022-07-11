720d88a268921addabede02c27b62772b0184729
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-11 03:31:42 -0300

    feat: add new props to button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,12 @@
 interface IButton {
+  type?: "button" | "submit" | "reset"
+  id?: string
   onClick: () => void;
   text: string;
 }
 
-const Button = (props: IButton) => {
-  return <button data-testid='btn' onClick={props.onClick}>{props.text}</button>;
+const Button = ({ id, type, onClick, text }: IButton) => {
+  return <button id={id ?? ''} type={type ?? 'button'} data-testid='btn' onClick={onClick}>{text}</button>;
 };
 
 export default Button;
