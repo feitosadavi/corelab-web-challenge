3d58d73212e68ece3c0d8bb34d7fe3ece187b9d0
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-11 15:21:13 -0300

    refactor: remove unused import

--- src/pages/AddVehicle/AddVehicle.test.tsx
+++ src/pages/AddVehicle/AddVehicle.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AddVehicle from '.';
-import userEvent from '@testing-library/user-event';
 
 describe('AddVehicle component tests', () => {
   const makeSut = () => {
